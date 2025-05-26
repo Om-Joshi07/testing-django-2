dc98d67626f54a8067d1db42ca34297e680dfb81
Om-Joshi07 <omjoshi424566@gmail.com>
2025-05-26 20:17:49 +0545

    modified adapters.py

--- krishipath/logins/adapter.py
+++ krishipath/logins/adapter.py
@@ -8,7 +8,7 @@ class CustomAccountAdapter(DefaultAccountAdapter):
         return '/logins/register/'
 
     def get_logout_redirect_url(self, request):
-        return '/logins/login/'
+        return '/'
 
     def get_signup_redirect_url(self, request):
         return '/logins/register/'
